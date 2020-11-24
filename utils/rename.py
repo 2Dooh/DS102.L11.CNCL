67c170a1633bc0345dbafadd0891f609f1a4250d
2Dooh <tudo@DESKTOP-VNDBCH6.localdomain>
2020-11-24 18:54:03 +0700

    update rename util, change annotation names

---
+++ utils/rename.py
@@ -0,0 +1,24 @@
+import os
+import re
+import click
+
+@click.command()
+@click.option('--extension', '-ext', required=True)
+@click.option('--path', '-p', required=True)
+def cli(extension, path):
+
+    path = path
+
+    images = os.listdir(path)
+
+    pattern = re.compile('(\d+).{}'.format(extension))
+
+    indices = list(map(lambda img: int(re.findall(pattern, img)[0]), images))
+
+    for idx, img in zip(indices, images):
+        filename = os.path.join(path, img)
+        replaced_name = os.path.join(path, '{:0>3d}.{}'.format(idx, extension))
+        os.replace(filename, replaced_name)
+
+if __name__ == "__main__":
+    cli()
