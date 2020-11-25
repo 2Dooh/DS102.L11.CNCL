e91d8301f034bb2df97b7cc9aab9500f76c24f56
hoangtuan-hcmuit <tuanhoang.uit@gmail.com>
2020-11-25 21:53:11 +0700

    tuan fix exact_img

--- utils/exact_img.py
+++ utils/exact_img.py
@@ -3,55 +3,37 @@ import json
 import xmltodict
 import matplotlib.pyplot as plt
 import os
+
 annot_path = '../data/labeled_images'
 img_path = '../img'
-# i = 0
 annotations = os.listdir(annot_path)
-for annot in annotations:
+for idx, annot in enumerate(annotations):
+    filename = annot.replace('.xml', '')
     with open(os.path.join(annot_path, annot)) as f:
         annot_dict = xmltodict.parse(f.read()) #read giong doan nay ne`
-        # print(annot_dict)
         json_data = json.dumps(annot_dict)
-    with open('test.json', 'w+') as writer:
-        writer.write(json_data)
-        writer.close()
-        with open('test.json', 'r+') as file:
-            annotation = json.load(file) 
-            # for i in len(annotation['annotation']['object']):
-            object = annotation['annotation']['object']
-            for i in len(object):
-                
-            label = object['name']
-            coordinate = object['bndbox']
-            xmin, ymin, xmax, ymax = [int(coord) for coord in coordinate.values()] 
-            img = cv.imread(img_path)
+        annotation = json.loads(json_data) 
+        objects = annotation['annotation']['object']
+        for j, dic in enumerate(objects):
+            label = dic['name']
+            coordinate = dic['bndbox']
+            [xmin, ymin, xmax, ymax] = [int(coord) for coord in coordinate.values()]
+            img = cv.imread(os.path.join(img_path, filename + '.jpg'))
             cropped_img = img[ymin:ymax, xmin:xmax]
-            save_path = label
+            resized_img = cv.resize(cropped_img, (200, 152), interpolation = cv.INTER_AREA)
+            save_path = '../data/imgs/{}'.format(label)
             if (not os.path.exists(save_path)):
                 os.makedirs(save_path)
-            cv.imwrite(os.path.join(save_path, '.jpg'.format(0)), cropped_img)
-        
-        # annotation = json_data['ann']
-  
-    # #     print(annot)
-    # # i+=1
-    # #     # json_file = 'test.json'
-    #     with open(json_file, 'w+') as file:
-    #     annotation = json.load(file)
-    #     object = annotation['annotation']['object']
-    #     label = object['name']
-    #     coordinate = object['bndbox']
-
-    #     xmin, ymin, xmax, ymax = [int(coord) for coord in coordinate.values()] 
-
-    #     img = cv.imread(img_path)
-    #     cropped_img = img[ymin:ymax, xmin:xmax]
-
-    #     save_path = label
-
-    #     if (not os.path.exists(save_path)):
-    #         os.makedirs(save_path)
-
-    #     cv.imwrite(os.path.join(save_path, '.jpg'.format(0)), cropped_img)
-
-    
+            cv.imwrite(os.path.join(save_path, '{}_{}.jpg'.format(idx, j)), resized_img)
+            
+            # label = [sub['name'] for sub in object]
+            # label = object['name']
+            # coordinate = [sub['bndbox'] for sub in object]
+            # coordinate = object['bndbox']
+            # xmin, ymin, xmax, ymax = [int(coord) for coord in coordinate.values()] 
+            # img = cv.imread(img_path)
+            # cropped_img = img[ymin:ymax, xmin:xmax]
+            # save_path = label
+            # if (not os.path.exists(save_path)):
+            #     os.makedirs(save_path)
+            # cv.imwrite(os.path.join(save_path, '.jpg'.format(0)), cropped_img)
